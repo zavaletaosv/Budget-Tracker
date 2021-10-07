4313f7ab9986eef973260ba2577a9dab808d06b2
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-07 08:13:19 -0500

    public/db.js updated: created an object store called BudgetStore

--- public/db.js
+++ public/db.js
@@ -1,3 +1,8 @@
 let db;
 
 const request = indexedDB.open('BudgetDB');
+
+request.onupgradeneeded = function (e) {
+    db = e.target.result;
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+}
