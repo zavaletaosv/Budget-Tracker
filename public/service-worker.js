faa39a0a421de5ae84c5265ddc134b6dd31691a6
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-07 15:45:50 -0500

    public/service-worker.js updated: added fetch function

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,19 +25,58 @@ self.addEventListener('activate', e => {
     const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     e.waitUntil(
         caches
-        .keys()
-        .then(cacheNames => {
-            return cacheNames.filter(
-                cacheName => !currentCaches.includes(cacheName)
-            );
+            .keys()
+            .then(cacheNames => {
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
+            })
+            .then(cachesToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cachesToDelete => {
+                        return caches.delete(cachesToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", e => {
+    if (
+        e.request.method !== "GET" ||
+        !e.request.url.startsWith(self.location.origin)
+    ) {
+        e.respondWith(fetch(e.request));
+        return;
+    }
+
+    if (e.request.url.includes("/api/transaction")) {
+        e.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(e.request)
+                    .then(response => {
+                        cache.put(e.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(e.request));
+            })
+        );
+        return;
+    }
+
+    e.respondWith(
+        caches.match(e.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(e.request).then(response => {
+                    return cache.put(e.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
         })
-        .then(cachesToDelete => {
-            return Promise.all(
-                cachesToDelete.map(cachesToDelete => {
-                    return caches.delete(cachesToDelete);
-                })
-            );
-        })
-        .then(() => self.clients.claim())
     );
 });
