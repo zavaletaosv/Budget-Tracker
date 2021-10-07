ba50f872a815462ada0f8529b7d390dafd2faf59
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-07 08:41:38 -0500

    public/db.js updated: function to check database created

--- public/db.js
+++ public/db.js
@@ -24,3 +24,28 @@ function saveRecord(record) {
     const store = transaction.objectStore('BudgetStore');
     store.add(record);
 }
+
+function checkDatabase() {
+    const transaction = db.transaction(['BudgetStore', 'readwrite']);
+    const store = transaction.objectStore('BudgetStore');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => response.json())
+            .then(() => {
+                const transaction = db.transaction(['BudgetStore', 'readwrite']);
+                const store = transaction.objectStore('BudgetStore');
+                store.clear();
+            });
+        }
+    };
+}
