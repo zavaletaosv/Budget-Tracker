d4da2e29dc1b69744fcabddb908ad407c2bc745a
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-07 08:58:16 -0500

    public/service-worker.js updated: files added to cache

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/db.js',
+    '/index.html',
+    'index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
