38e5368df088852b94b481d174c0c0891f02d76b
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-07 08:22:28 -0500

    public/db.js updated: requests created

--- public/db.js
+++ public/db.js
@@ -5,4 +5,16 @@ const request = indexedDB.open('BudgetDB');
 request.onupgradeneeded = function (e) {
     db = e.target.result;
     db.createObjectStore('BudgetStore', { autoIncrement: true });
+};
+
+request.onsuccess = function (e) {
+    db = e.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (e) {
+    console.log(e.target.errorCode);
 }
